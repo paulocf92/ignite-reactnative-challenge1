20f3b52f330b985b873998250c9b9c5448dcb0d0
Paulo Filho <pcf.feat@gmail.com>
2021-05-19 15:08:03 -0400

    challenge done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,40 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task if it's not empty
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleMarkTaskAsDone(id: number) {
     //TODO - mark task as done if exists
+    const newTasks = tasks.map(task =>
+      task.id === id
+        ? {
+            ...task,
+            done: !task.done,
+          }
+        : task
+    );
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    const newTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(newTasks);
   }
 
   return (
@@ -31,11 +53,11 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <MyTasksList
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
-  )
+  );
 }
